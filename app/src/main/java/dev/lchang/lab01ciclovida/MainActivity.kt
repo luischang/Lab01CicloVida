b2398d0bd13eaab5f1b5b5cf8e92e0e1f0d2975b
laboratorioesan <laboratorioesan@esan.edu.pe>
2025-03-29 10:35:59 -0500

    Initial commit

---
+++ app/src/main/java/dev/lchang/lab01ciclovida/MainActivity.kt
@@ -0,0 +1,43 @@
+package dev.lchang.lab01ciclovida
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+        private val tag: String = "INF-STATE"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.i(tag,"MainActivity-->onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag,"MainActivity-->onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag,"MainActivity-->onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag,"MainActivity-->onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag,"MainActivity-->onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag,"MainActivity-->onDestroy")
+    }
+}
